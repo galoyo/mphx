7c6916d1d60e6921a4f05de51a026bab8a252edf
galoyo <26208152+galoyo@users.noreply.github.com>
2018-01-06 18:16:06 -0800

    removed lag

--- example/haxeflixel/movement/server/Main.hx
+++ example/haxeflixel/movement/server/Main.hx
@@ -1,4 +1,5 @@
 package ;
+import mphx.server.impl.Server;
 
 typedef Player = {
 	x: Int,
@@ -6,21 +7,24 @@ typedef Player = {
 	id: String
 }
 
-class Main {
+class Main{
 	public var players:Map<mphx.connection.IConnection,Player>;
-
+	private var ip:String;
+	private var s:Server;
+	
 	public function new (){
+
 		players = new Map();
-
-		var ip = "127.0.0.1";
+		
+		ip = "127.0.0.1";
+		s = new mphx.server.impl.Server(ip, 8000);
+		
 		if (Sys.args()[0] != null) ip = Sys.args()[0];
-
-		var s = new mphx.server.impl.Server(ip,8000);
 
 		s.events.on("Join",function(data:Dynamic,sender:mphx.connection.IConnection){
 
 			players.set(sender,data);
-
+			trace("A new player has joined from client.");
 			s.broadcast("New Player",data);
 		});
 
@@ -31,9 +35,23 @@ class Main {
 			s.broadcast("Player Move",player);
 		});
 
-		s.start();
-	}
+		s.events.on("Update",function (data,sender){
+			var player = players.get(sender);
+		
+			s.broadcast("Player Move",player);
+		});
 
+		s.events.on("Disconnect",function (data,sender){
+			var player = players.get(sender);
+
+			s.broadcast("Disconnect",player);
+		});
+
+				
+		s.start();
+		
+	}
+	
 	function makeID () {
 		var id = "";
 		var charactersToUse = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
